b09c30b8358dc80ca52502a81ae7fb93a23d2fe8
Shashank Manohar <shashankmanohar1734@gmail.com>
2026-02-04 12:32:08 +0530

    api update

--- src/lib/blogApi.js
+++ src/lib/blogApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://teamexcellentcareerinstitute.in/api/blogs';
+// Use environment variable for API URL
+const API_URL = `${import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'}/blogs`;
 
 // Get auth token from localStorage
 const getAuthToken = () => {
